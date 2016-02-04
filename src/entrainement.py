0c2cdbf229eff131a05de474de83641d2d30c226
BritMag <britanniemag@gmail.com>
2016-02-04 19:07:54 +0100

    Added argument parsing

--- src/entrainement.py
+++ src/entrainement.py
@@ -2,6 +2,7 @@
 import numpy as np
 from scipy import stats
 from random import shuffle
+from optparse import OptionParser
 from bm_io import read_data_file, send_message
 from bm_prob import initialize_named_choices
 
@@ -14,12 +15,12 @@ def training_times(data_dict, max_time):
     weights = np.array([data_dict[k][2] for k in keys])
     rv = initialize_named_choices(keys, weights)
 
-    sequence = rv.rvs(size=10)
+    sequence = rv.rvs(size=100)
     
 
     time_left = max_time
 
-    text =  "\nEntrainement sur %d minutes : \n" % max_time
+    text =  "\nSéance sur %d minutes : \n" % max_time
     for s in sequence:
         if time_left > 0:
             key = keys[s]
@@ -35,17 +36,25 @@ def training_times(data_dict, max_time):
 
 if __name__ == '__main__':
 
-    duree = 15
-    data_dict = read_data_file('positions_britmag.txt')
-    text = training_times(data_dict, duree)
+    usage = "usage: %prog [options] arg1 arg2"
+    parser = OptionParser(usage=usage)
+    parser.add_option("-f", "--fich", dest="filename",
+                      help="Nom fichier avec options, durees, poids.",
+                      action="store", type="string")
+    parser.add_option("-t", "--dur", dest="duree",
+                      help="Duree (en minutes) de la seance.",
+                      action="store", type="int")
+    parser.add_option("-e", "--email", dest="e_filename",
+                      help="Nom fichier avec infos pour envoi email.",
+                      action="store", type="string")
 
-    duree = 30
-    data_dict = read_data_file('accessoires_britmag.txt')
-    text = text + training_times(data_dict, duree)
+    (options, args) = parser.parse_args()
 
-    duree = 10
-    data_dict = read_data_file('plaisirs_britmag.txt')
-    text = text + training_times(data_dict, duree)
+    data_dict = read_data_file(options.filename)
+    text = training_times(data_dict, options.duree)
 
-    send_message('email_entrainement.txt', text, "Séquence d'entrainement")
+    if options.e_filename is not None:
+        send_message(options.e_filename, text, "Séance d'entrainement")
+    else:
+        print text
 
